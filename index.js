ad822ef58cc7f3bbf6d682e9f97e1a0fd27d51dc
Danielle Man <man.danielleh@gmail.com>
2020-09-23 20:40:19 -0700

    simplify main server file

--- index.js
+++ index.js
@@ -1,21 +1,13 @@
-const { ApolloServer, gql } = require('apollo-server');
+const fs = require("fs");
+const { ApolloServer, gql } = require("apollo-server");
 
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
 const typeDefs = gql`
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
-
-  # This "Book" type defines the queryable fields for every book in our data source.
   type Book {
     title: String
     author: String
     description: String
   }
 
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
   type Query {
     books: [Book]
   }
@@ -23,32 +15,30 @@ const typeDefs = gql`
 
 const books = [
   {
-    title: 'The Awakening',
-    author: 'Kate Chopin',
+    title: "The Awakening",
+    author: "Kate Chopin",
+    review: "Pretty good.",
   },
   {
-    title: 'City of Glass',
-    author: 'Paul Auster',
+    title: "City of Glass",
+    author: "Paul Auster",
+    review: "A little hard to discern.",
   },
 ];
 
-// Resolvers define the technique for fetching the types defined in the
-// schema. This resolver retrieves books from the "books" array above.
 const resolvers = {
   Query: {
     books: () => books,
   },
 };
 
-// The ApolloServer constructor requires two parameters: your schema
-// definition and your set of resolvers.
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
 
-// The `listen` method launches a web server.
 server.listen().then(({ url }) => {
-  console.log(`🚀  Server running at ${url}\n📭  Run queries at https://studio-staging.apollographql.com/dev`);
+  console.log(`🚀  Server is running!
+📭  Query at https://studio-staging.apollographql.com/dev
+🔉  Listening at ${url}`);
 });
-
