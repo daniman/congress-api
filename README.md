2c7b6511a1f85546fa3c2836c0dc5ccb07ee8b2f
Danielle Man <man.danielleh@gmail.com>
2020-10-04 19:36:24 -0700

    start wrapping propublica

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
-The lightest-weight GraphQL server you'll find.
+A GraphQL server (Apollo Server) to wrap the ProPublica Congress API: https://projects.propublica.org/api-docs/congress-api/.
 
-To run:
+My intent with this project is to work through what's available in the ProPublica API and selectively add things to this API.
+
+The server is deployed automatically to Heroku from `main`.
+
+### To develop
+
+Get your API Key from ProPublica (free, super easy): https://www.propublica.org/datastore/api/propublica-congress-api.
+
 ```
 npm install
-npm start
+PROPUBLICA_KEY=<YOUR_KEY> npm start:dev
 ```
--- index.js
+++ index.js
@@ -1,35 +1,63 @@
 const fs = require("fs");
 const { ApolloServer, gql } = require("apollo-server");
+const fetch = require("node-fetch");
 
-const typeDefs = gql`
-  type Book {
-    title: String
-    author: String
-    description: String
-    hello: String
-  }
+const typeDefs = fs.readFileSync("./schema.graphql", "utf8").toString();
 
-  type Query {
-    books: [Book]
-  }
-`;
+const headers = {
+  "X-API-Key": process.env.PROPUBLICA_KEY || "FAKE_KEY",
+};
 
-const books = [
-  {
-    title: "The Awakening",
-    author: "Kate Chopin",
-    review: "Pretty good.",
-  },
-  {
-    title: "City of Glass",
-    author: "Paul Auster",
-    review: "A little hard to discern.",
-  },
-];
+const toCamel = (s) => {
+  return s.replace(/([-_][a-z])/gi, ($1) => {
+    return $1.toUpperCase().replace("-", "").replace("_", "");
+  });
+};
+
+const snakeToCamel = (object) => {
+  const newObj = {};
+  Object.entries(object).forEach(([key, value]) => {
+    newObj[toCamel(key)] =
+      value instanceof Array
+        ? value.map((item) => snakeToCamel(item))
+        : !!value && typeof value === "object"
+        ? snakeToCamel(value)
+        : value;
+  });
+  return newObj;
+};
 
 const resolvers = {
   Query: {
-    books: () => books,
+    congress: async (_, args) => {
+      if (!args.congress)
+        throw new Error("Congress session must be specified, eg. 116");
+      if (!args.chamber)
+        throw new Error("Congress chamber must be specified, eg. SENATE");
+
+      const data = await fetch(
+        `https://api.propublica.org/congress/v1/${
+          args.congress
+        }/${args.chamber.toLowerCase()}/members.json`,
+        {
+          headers,
+        }
+      )
+        .then((res) => res.json())
+        .catch((err) => new Error(err));
+      return data.results.map((res) => snakeToCamel(res));
+    },
+    memberById: async (_, args) => {
+      const data = await fetch(
+        `https://api.propublica.org/congress/v1/members/${args.id}.json`,
+        {
+          headers,
+        }
+      )
+        .then((res) => res.json())
+        .catch((err) => new Error(err));
+      return snakeToCamel(data.results[0]);
+    },
   },
 };
 
