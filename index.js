25a8bc14116cd271c94fc5c883a019288703854c
Danielle Man <man.danielleh@gmail.com>
2020-10-26 22:27:26 -0700

    enable tracing

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const{ ApolloServerPluginInlineTrace } = require("apollo-server-core");
 const { ApolloServer, gql } = require("apollo-server");
 const fetch = require("node-fetch");
 
@@ -7,8 +8,6 @@ const typeDefs = fs.readFileSync("./schema.graphql", "utf8").toString();
 const headers = {
   "X-API-Key": process.env.PROPUBLICA_KEY || "FAKE_KEY",
 };
-
-console.log(process.env.PROPUBLICA_KEY);
 
 const toCamel = (s) => {
   return s.replace(/([-_][a-z])/gi, ($1) => {
@@ -78,19 +77,84 @@ const resolvers = {
         .catch((err) => new Error(err));
     },
   },
+  MemberVote: {
+    vote: async (parent) => {
+      if (!parent.voteUri)
+        throw new Error("Cannot fetch vote without MemberVote.voteUri");
+
+      return await fetch(parent.voteUri, {
+        headers,
+      })
+        .then(async (res) => {
+          if (res.ok) {
+            const data = await res.json();
+            return snakeToCamel(data.results.votes.vote);
+          } else {
+            throw new Error("Error fetching data. Did you include an API Key?");
+          }
+        })
+        .catch((err) => new Error(err));
+    },
+  },
+  MemberDetails: {
+    votes: async (parent, args) => {
+      if (!parent.id)
+        throw new Error(
+          "Cannot fetch member vote data if Member.id is not requested."
+        );
+
+      return await fetch(
+        `https://api.propublica.org/congress/v1/members/${parent.id}/votes.json?offset=${args.offset}`,
+        {
+          headers,
+        }
+      )
+        .then(async (res) => {
+          if (res.ok) {
+            const data = await res.json();
+            return data.results[0].votes.map((res) => snakeToCamel(res));
+          } else {
+            throw new Error("Error fetching data. Did you include an API Key?");
+          }
+        })
+        .catch((err) => new Error(err));
+    },
+  },
+  Bill: {
+    details: async (parent, args) => {
+      if (!parent.apiUri)
+        throw new Error(
+          "Cannot fetch member vote data if Member.id is not requested."
+        );
+
+      return await fetch(parent.apiUri, {
+        headers,
+      })
+        .then(async (res) => {
+          if (res.ok) {
+            const data = await res.json();
+            console.log(data);
+          } else {
+            throw new Error("Error fetching data. Did you include an API Key?");
+          }
+        })
+        .catch((err) => new Error(err));
+    },
+  },
 };
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  plugins: [ApolloServerPluginInlineTrace()],
 });
 
 server
   .listen({
     port: process.env.PORT || 4000,
   })
-  .then(({ url }) => {
+  .then(({ port }) => {
     console.log(`🚀  Server is running!
 📭  Query at https://studio.apollographql.com/dev
-🔉  Listening at ${url}`);
+🔉  Listening on port ${port}`);
   });
